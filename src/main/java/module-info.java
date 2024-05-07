b7d0371e231145831ec8517ac52a5b9a9c7cd7a2
allison hincapie <allison.hincapier@uqvirtual.edu.co>
2024-05-07 14:08:19 -0500

    Se sube el proyecto inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module uniquindio.edu.co.proyectofinal3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens uniquindio.edu.co.proyectofinal3 to javafx.fxml;
+    exports uniquindio.edu.co.proyectofinal3;
+}
