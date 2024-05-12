fdccdf94b3c5867ce61947f5d07f07e8b3ee0d22
Joseph Yulian Garcia <josephy.garciac@uqvirtual.edu.co>
2024-05-11 23:31:35 -0500

    Update
    InicioSesionView y el Controlador de este

--- src/main/java/uniquindio/edu/co/proyectofinal3/Controllers/InicioSesionController.java
+++ src/main/java/uniquindio/edu/co/proyectofinal3/Controllers/InicioSesionController.java
@@ -1,4 +1,25 @@
 package uniquindio.edu.co.proyectofinal3.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class InicioSesionController {
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private PasswordField contrasenia;
+
+    @FXML
+    private TextField identificacion;
+
+    @FXML
+    void btnIngresarClicked(ActionEvent event) {
+
+    }
+
 }
