21ce7458e81f2f0215e0d219a38e937d4e6afdd5
JuanEstebanVictoria <juane.victorial@uqvirtual.edu.co>
2024-05-13 19:34:16 -0500

    Archivos paquete

---
+++ src/main/java/uniquindio/edu/co/proyectofinal3/Archivos/ArchivoUtils.java
@@ -0,0 +1,20 @@
+package uniquindio.edu.co.proyectofinal3.Archivos;
+
+import java.io.*;
+
+public class ArchivoUtils {
+    public static void serializarObjeto(String ruta, Object objeto) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta));
+        oos.writeObject(objeto);
+        oos.close();
+    }
+    public static Object deserializarObjeto(String ruta) throws Exception{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta));
+        Object objeto = ois.readObject();
+        ois.close();
+        return objeto;
+    }
+
+    
+
+}
