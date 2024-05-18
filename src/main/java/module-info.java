717d644c29bca8d80b2c0c07e8e44ab437ebd09b
Juan Camilo Sanchez Cano <juanc.sanchezc1@uqvirtual.edu.co>
2024-05-18 18:43:09 -0500

    Corrección al modulo
    ))CA/LI((

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,5 @@ module uniquindio.edu.co.proyectofinal3 {
     opens uniquindio.edu.co.proyectofinal3 to javafx.fxml;
     exports uniquindio.edu.co.proyectofinal3.Controllers;
     opens uniquindio.edu.co.proyectofinal3.Controllers to javafx.fxml;
+    exports uniquindio.edu.co.proyectofinal3 to javafx.graphics;
 }
