f5a4fe2520600bfe164ba5e534f9df6122ffaf91
Joseph Yulian Garcia <josephy.garciac@uqvirtual.edu.co>
2024-05-18 15:45:43 -0500

    ...

--- src/main/java/uniquindio/edu/co/proyectofinal3/HelloApplication.java
+++ src/main/java/uniquindio/edu/co/proyectofinal3/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
         stage.show();
         stage.setResizable(false);
     }
-
+    
     public static void main(String[] args) {
         launch();
     }
