0d5d17022d362184d4a7ad23beff5a09d067cc43
Juan Camilo Sanchez Cano <juanc.sanchezc1@uqvirtual.edu.co>
2024-05-18 13:36:03 -0500

    Excepciones
    Excepciones faltantes de cliente y evento

---
+++ src/main/java/uniquindio/edu/co/proyectofinal3/Exepciones/DatosInvalidosEventoException.java
@@ -0,0 +1,8 @@
+package uniquindio.edu.co.proyectofinal3.Exepciones;
+
+public class DatosInvalidosEventoException extends Exception{
+    public DatosInvalidosEventoException(String mensaje) {
+
+        super(mensaje);
+    }
+}
---
+++ src/main/java/uniquindio/edu/co/proyectofinal3/Exepciones/EventoNoExisteException.java
@@ -0,0 +1,8 @@
+package uniquindio.edu.co.proyectofinal3.Exepciones;
+
+public class EventoNoExisteException extends Exception{
+
+    public EventoNoExisteException(String mensaje){
+        super(mensaje);
+    }
+}
